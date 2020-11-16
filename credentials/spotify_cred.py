67224f28aa833c914da77fc48293902ad02b0702
Nick Foote <68865367+Nick-foote@users.noreply.github.com>
2020-11-16 08:58:30 +0000

    Update spotify_cred.py

--- credentials/spotify_cred.py
+++ credentials/spotify_cred.py
@@ -1,9 +1,11 @@
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 
-### Hard-coded in
-client_id = "9f660f6108d44080b18f15b3eda4a432"
-client_secret = "8221d6445e064eeebb8a2d7439f0aa58"
+# -- Spotify API info below --
+client_id = "####"      
+client_secret = "####"
+
+
 redirect_uri = 'http://localhost:7777/callback'
 scope = 'user-library-read'
 
